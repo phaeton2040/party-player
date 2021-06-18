443dbbacf842bb2ca0416f4337b6c27c663402c5
phaeton2040 <phaeton2040@gmail.com>
2021-06-18 15:11:17 +0300

    Basic styling and component placeholders

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { faPlay } from "@fortawesome/free-solid-svg-icons/faPlay";
-import { faStepBackward } from "@fortawesome/free-solid-svg-icons/faStepBackward";
-import { faStepForward } from "@fortawesome/free-solid-svg-icons/faStepForward";
 
 @Component({
   selector: 'app-home',
@@ -11,9 +8,7 @@ import { faStepForward } from "@fortawesome/free-solid-svg-icons/faStepForward";
 })
 export class HomeComponent implements OnInit {
 
-  playIcon = faPlay;
-  nextSongIcon = faStepForward;
-  prevSongIcon = faStepBackward;
+  value = '';
 
   constructor(private router: Router) { }
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,10 +7,34 @@ import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { MatSliderModule } from "@angular/material/slider";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { HeaderComponent } from './components/header/header.component';
+import { SongsListComponent } from './components/songs-list/songs-list.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
-  declarations: [ HomeComponent ],
-  imports: [ CommonModule, SharedModule, HomeRoutingModule, FontAwesomeModule, MatSliderModule ]
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    SongsListComponent,
+    MainComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    HomeRoutingModule,
+    FontAwesomeModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatExpansionModule
+  ]
 })
 export class HomeModule {
 }
